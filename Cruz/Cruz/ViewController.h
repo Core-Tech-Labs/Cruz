a606fcf6ee9803c4f0e14173c5e74a78e1a706bf
Rudy Jessop <rudy@pcjunkii.com>
2014-10-15 17:14:20 -0400

    inaugural commit

---
+++ Cruz/Cruz/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Cruz
+//
+//  Created by Rudy on 10/15/14.
+//  Copyright (c) 2014 Core Tech Labs, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
