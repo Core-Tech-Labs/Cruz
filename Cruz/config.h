424e09f3f877406beef064114b693c2fe5155998
Rudy Jessop <rudyjessop@gmail.com>
2014-12-06 13:52:24 -0500

    RabbitMQ library/ supporting files commit

---
+++ Cruz/config.h
@@ -0,0 +1,20 @@
+//
+//  config.h
+//  Cruz
+//
+//  Created by Rudy on 12/6/14.
+//  Copyright (c) 2014 Core Tech Labs, Inc. All rights reserved.
+//
+
+#ifndef Cruz_config_h
+#define Cruz_config_h
+
+#define VERSION "0.3.0"
+
+#ifndef __cplusplus
+#define inline inline
+#endif
+
+/* #undef HAVE_HTONLL */
+
+#endif
